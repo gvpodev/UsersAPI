bc82e630ed1685a32b124ac6c811c2f35dbfe147
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-06-30 19:56:50 -0300

    config cors policy

--- UsersAPI.Services/Extensions/CorsExtensions.cs
+++ UsersAPI.Services/Extensions/CorsExtensions.cs
@@ -1,6 +1,26 @@
 ﻿namespace UsersAPI.Api.Extensions
 {
-    public class CorsExtensions
+    public static class CorsExtensions
     {
+        private static string _policyName = "DefaultPolicy";
+
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(s => s.AddPolicy(_policyName, builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+            }));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCorsPolicy(this IApplicationBuilder app)
+        {
+            app.UseCors(_policyName);
+
+            return app;
+        }
     }
 }
--- UsersAPI.Services/Program.cs
+++ UsersAPI.Services/Program.cs
@@ -6,10 +6,12 @@ builder.Services.AddControllers();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddSwaggerDoc();
 builder.Services.AddJwtBearer();
+builder.Services.AddCorsPolicy();
 
 var app = builder.Build();
 
 app.UseSwaggerDoc();
+app.UseCorsPolicy();
 app.UseAuthentication();
 app.UseHttpsRedirection();
 app.UseAuthorization();
