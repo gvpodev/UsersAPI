10e4209bce671f535e9b1cfbb60078d06f1296b6
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-06-30 21:29:57 -0300

    add datacontext

---
+++ UsersAPI.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersAPI.Domain.Models;
+
+namespace UsersAPI.Infra.Data.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "db_users");
+        }
+    }
+}
