ba5da2dfb56134df91914a9c3ad73ebc33c0d76c
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-03 18:46:55 -0300

    add user service at domain layer

---
+++ UsersAPI.Domain/Interfaces/Services/IUserDomainService.cs
@@ -0,0 +1,14 @@
+﻿using UsersAPI.Domain.Models;
+
+namespace UsersAPI.Domain.Interfaces.Services
+{
+    public interface IUserDomainService : IDisposable
+    {
+        void Add(User user);
+        void Update(User user);
+        void Delete(User user);
+        User? Find(Guid id);
+        User? Find(string email);
+        User? Find(string email, string password);
+    }
+}
---
+++ UsersAPI.Domain/Services/UserDomainService.cs
@@ -0,0 +1,55 @@
+﻿using UsersAPI.Domain.Interfaces.Repositories;
+using UsersAPI.Domain.Interfaces.Services;
+using UsersAPI.Domain.Models;
+
+namespace UsersAPI.Domain.Services
+{
+    public class UserDomainService : IUserDomainService
+    {
+        private readonly IUnitOfWork? _unitOfWork;
+
+        public UserDomainService(IUnitOfWork? unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Add(User user)
+        {
+            _unitOfWork?.UserRepository.Add(user);
+            _unitOfWork?.SaveChanges();
+        }
+
+        public void Delete(User user)
+        {
+            _unitOfWork?.UserRepository.Delete(user);
+            _unitOfWork?.SaveChanges();
+        }
+
+        public User? Find(Guid id)
+        {
+            return _unitOfWork?.UserRepository.FindById(id);
+        }
+
+        public User? Find(string email)
+        {
+            return _unitOfWork?.UserRepository.Find(u => u.Email.Equals(email));
+        }
+
+        public User? Find(string email, string password)
+        {
+            return _unitOfWork?.UserRepository.Find(u => u.Email.Equals(email)
+                && u.Password.Equals(password));
+        }
+
+        public void Update(User user)
+        {
+            _unitOfWork?.UserRepository.Update(user);
+            _unitOfWork?.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork?.Dispose();
+        }
+    }
+}
