56db8ca79aa09d8cb4a90ce76243100a33997e53
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-03 20:00:12 -0300

    add app services interfaces

---
+++ UsersAPI.Application/Interfaces/Application/IAuthAppService.cs
@@ -0,0 +1,12 @@
+﻿using UsersAPI.Application.Dtos.Requests;
+using UsersAPI.Application.Dtos.Responses;
+
+namespace UsersAPI.Application.Interfaces.Application
+{
+    public interface IAuthAppService : IDisposable
+    {
+        LoginResponseDto Login(LoginRequestDto request);
+        UserResponseDto ForgotPassword(ForgotPasswordRequestDto request);
+        UserResponseDto ResetPassword(ResetPasswordRequestDto request);
+    }
+}
