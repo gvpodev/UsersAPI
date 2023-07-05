a885c557e0bba84ea84ee7c5a11ca84fc1dd9785
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-05 19:14:28 -0300

    mapping user to db

--- UsersAPI.Infra.Data/Contexts/DataContext.cs
+++ UsersAPI.Infra.Data/Contexts/DataContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UsersAPI.Domain.Models;
+using UsersAPI.Infra.Data.Configurations;
 
 namespace UsersAPI.Infra.Data.Contexts
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
         
         public DbSet<User> Users { get; set; }
     }
