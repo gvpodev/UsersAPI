cc83e82e244b787990b6e77c8daac81cdb4ef062
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-07 19:54:37 -0300

    implement user message producer

--- UsersAPI.Infra.Messages/Producers/UserMessageProducer.cs
+++ UsersAPI.Infra.Messages/Producers/UserMessageProducer.cs
@@ -1,13 +1,54 @@
-﻿using UsersAPI.Domain.Interfaces.Messages;
+﻿using System.Text;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using UsersAPI.Domain.Interfaces.Messages;
 using UsersAPI.Domain.ValueObjects;
+using UsersAPI.Infra.Messages.Settings;
 
 namespace UsersAPI.Infra.Messages.Producers
 {
     public class UserMessageProducer : IUserMessageProducer
     {
+        private readonly RabbitMQSettings? _rabbitMqSettings;
+
+        public UserMessageProducer(IOptions<RabbitMQSettings?> rabbitMqSettings)
+        {
+            _rabbitMqSettings = rabbitMqSettings.Value;
+        }
+
         public void Send(UserMessageVO userMessage)
         {
-            throw new NotImplementedException();
+            if (_rabbitMqSettings?.Url != null)
+            {
+                var connectionFactory = new ConnectionFactory
+                {
+                    Uri = new Uri(_rabbitMqSettings?.Url)
+                };
+                
+                // conecta no rabbit mq
+                using (var connection = connectionFactory.CreateConnection())
+                {
+                    // cria uma fila
+                    using (var model = connection.CreateModel())
+                    {
+                        model.QueueDeclare(
+                            queue: _rabbitMqSettings.Queue,
+                            durable: true,
+                            autoDelete: false,
+                            exclusive: false,
+                            arguments: null
+                        );
+                        
+                        model.BasicPublish(
+                            exchange: string.Empty,
+                            routingKey: _rabbitMqSettings.Queue,
+                            basicProperties: null,
+                            body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(userMessage))
+                        );
+                    }
+                }
+            }
         }
     }
 }
