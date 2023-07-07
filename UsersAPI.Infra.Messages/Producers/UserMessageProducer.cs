72ccdf8a23bf078856c791cea1a04f3a0e615ddf
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-07 19:38:34 -0300

    add rabbit mq setting and create producer class

---
+++ UsersAPI.Infra.Messages/Producers/UserMessageProducer.cs
@@ -0,0 +1,13 @@
+﻿using UsersAPI.Domain.Interfaces.Messages;
+using UsersAPI.Domain.ValueObjects;
+
+namespace UsersAPI.Infra.Messages.Producers
+{
+    public class UserMessageProducer : IUserMessageProducer
+    {
+        public void Send(UserMessageVO userMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
