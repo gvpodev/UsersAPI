b02ce77d29f353cd7a0cd9356224fa6811b167d2
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-07 17:58:26 -0300

    add exceptions

---
+++ UsersAPI.Domain/Exceptions/EmailAlreadyExistsException.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersAPI.Domain.Exceptions
+{
+    public class EmailAlreadyExistsException : Exception
+    {
+        public EmailAlreadyExistsException(string email)
+            : base($"The e-mail {email} is already in use.")
+        {
+        }
+    }
+}
---
+++ UsersAPI.Services/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using UsersAPI.Api.Models;
+
+namespace UsersAPI.Api.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate? _requestDelegate;
+
+        public ExceptionMiddleware(RequestDelegate? requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch (ApplicationException ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        public async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            switch (exception)
+            {
+                case ApplicationException:
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case Exception:
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+
+            var errorResultModel = new ErrorResultModel
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = exception.Message
+            };
+
+            await context.Response.WriteAsync(errorResultModel.ToString());
+        }
+    }
+}
