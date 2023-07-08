9919260f1afe127f937cb27c4dcd98fbbc2ee381
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-07 21:41:44 -0300

    add integration test do create user

--- UsersAPI.Tests/UsersTests.cs
+++ UsersAPI.Tests/UsersTests.cs
@@ -1,11 +1,35 @@
-﻿namespace UsersAPI.Tests
+﻿using System.Net;
+using Bogus;
+using FluentAssertions;
+using UsersAPI.Application.Dtos.Requests;
+using UsersAPI.Application.Dtos.Responses;
+using UsersAPI.Tests.Helpers;
+
+namespace UsersAPI.Tests
 {
     public class UsersTests
     {
-        [Fact(Skip = "Not implemented")]
-        public void Users_Post_Returns_Ok()
+        [Fact]
+        public async Task Users_Post_Returns_Created()
         {
-            //TODO
+            var faker = new Faker("pt_BR");
+            var request = new UserAddRequestDto
+            {
+                Name = faker.Person.FullName,
+                Email = faker.Internet.Email(),
+                Password = "qhMWP{QoLf{^8&qeeHa<EqFk<",
+                ConfirmationPassword = "qhMWP{QoLf{^8&qeeHa<EqFk<"
+            };
+            var content = TestHelper.CreateContent(request);
+
+            var result = await TestHelper.CreateClient.PostAsync("api/users", content);
+
+            result.StatusCode.Should().Be(HttpStatusCode.Created);
+            var response = TestHelper.ReadResponse<UserResponseDto>(result);
+            response.Id.Should().NotBeEmpty();
+            response.Name.Should().Be(request.Name);
+            response.Email.Should().Be(request.Email);
+            response.CreatedAt.Should().NotBeNull();
         }
 
         [Fact(Skip = "Not implemented")]
